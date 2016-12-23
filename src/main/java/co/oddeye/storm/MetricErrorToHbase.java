2d6128b03ebd4ebc877e70a81f32bc8d9743440b
vahan <vahan@vahan-pc>
2016-12-23 12:55:02 +0400

    Delete old days

--- src/main/java/co/oddeye/storm/MetricErrorToHbase.java
+++ src/main/java/co/oddeye/storm/MetricErrorToHbase.java
@@ -9,10 +9,13 @@ import co.oddeye.core.OddeeyMetricMeta;
 import co.oddeye.core.OddeeyMetricMetaList;
 import co.oddeye.core.globalFunctions;
 import com.google.gson.JsonParser;
+import com.stumbleupon.async.Callback;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Map;
 import net.opentsdb.core.TSDB;
+import net.opentsdb.query.QueryUtil;
 import net.opentsdb.utils.Config;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang.ArrayUtils;
@@ -23,7 +26,12 @@ import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.hbase.async.DeleteRequest;
+import org.hbase.async.FilterList;
+import org.hbase.async.KeyRegexpFilter;
+import org.hbase.async.KeyValue;
 import org.hbase.async.PutRequest;
+import org.hbase.async.ScanFilter;
+import org.hbase.async.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 //import org.apache.hadoop.hbase.util.Bytes;
@@ -124,15 +132,63 @@ public class MetricErrorToHbase extends BaseRichBolt {
                     buffer.put((byte) level).putLong(time.getValue());
                 }
                 values[3] = buffer.array();
-                
-                
 
-                LOGGER.warn("metric:" + metric.getName() + " Host:" + metric.getTags().get("host").getValue() + " Err:" + metric.getTags().get("UUID").getValue() + " state:" + metric.getErrorState().getState() + " time:" + metric.getErrorState().getTime() + " daykey:" + Hex.encodeHexString(key) + " message:" + message);
+                LOGGER.info("metric:" + metric.getName() + " Host:" + metric.getTags().get("host").getValue() + " Err:" + metric.getTags().get("UUID").getValue() + " state:" + metric.getErrorState().getState() + " time:" + metric.getErrorState().getTime() + " daykey:" + Hex.encodeHexString(key) + " message:" + message);
                 PutRequest putlast = new PutRequest(errorshistorytable, key, "l".getBytes(), qualifiers, values);
                 globalFunctions.getSecindaryclient(clientconf).put(putlast);
             } else {
-                DeleteRequest delreq = new DeleteRequest(errorshistorytable, key, "l".getBytes());
-                globalFunctions.getSecindaryclient(clientconf).delete(delreq);
+                Scanner scanner = globalFunctions.getSecindaryclient(clientconf).newScanner(errorshistorytable);
+                scanner.setServerBlockCache(false);
+                scanner.setFamily("l".getBytes());
+                key = metric.getUUIDKey();
+                StringBuilder buffer = new StringBuilder();
+                buffer.append("(?s)^.{4}(");
+                buffer.append("\\Q");
+                QueryUtil.addId(buffer, key, true);
+//                for (int i = 0; i < key.length; i++) {
+//                    if (key[i] >= 32 && key[i] != 92 && key[i] != 127) {
+//                        buffer.append((char) key[i]);
+//                    } else {
+//                        String temp;
+//                        if (key[i] == 92) {
+//                            buffer.append("\\\\");
+//                        } else {
+//                            temp = String.format("\\x%02x", key[i]);
+//                            buffer.append(temp);
+//                        }
+//                    }
+//                }
+                buffer.append(")$");
+                final ArrayList<ScanFilter> filters = new ArrayList<>();
+                filters.add(new KeyRegexpFilter(buffer.toString()));
+                scanner.setFilter(new FilterList(filters));
+                LOGGER.debug("Select key:" + Hex.encodeHexString(key));
+                LOGGER.debug("Select key:" + buffer.toString());
+
+                class cb implements Callback<Object, ArrayList<ArrayList<KeyValue>>> {
+
+                    @Override
+                    public Object call(final ArrayList<ArrayList<KeyValue>> rows) {
+                        if (rows == null) {
+                            return null;
+                        }
+                        try {
+                            final ArrayList<KeyValue> kvs = rows.get(0);
+                            final KeyValue kv = kvs.get(0);
+                            final DeleteRequest delreq = new DeleteRequest(errorshistorytable, kv.key(), "l".getBytes());
+                            LOGGER.info("Delete key:" + Hex.encodeHexString(kv.key()));
+                            globalFunctions.getSecindaryclient(clientconf).delete(delreq);
+                            return scanner.nextRows(1).addCallback(this);
+                        } catch (AssertionError e) {
+                            throw new RuntimeException("Asynchronous failure", e);
+                        }
+                    }
+                }
+                try {
+                    scanner.nextRows(1).addCallback(new cb()).join();
+                } finally {
+                    scanner.close().join();
+                }
             }
 
             PutRequest puthistory = new PutRequest(errorshistorytable, key, "h".getBytes(), globalFunctions.getNoDayKey(metric.getErrorState().getTime()), ByteBuffer.allocate(1).put((byte) metric.getErrorState().getLevel()).array());
